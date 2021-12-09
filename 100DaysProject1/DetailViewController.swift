174da7a5400df71f5a584a08a1d24f8f8ad49eb4
Mark <markfalcone2@gmail.com>
2021-12-09 15:08:44 -0800

    add sharing

--- 100DaysProject1/DetailViewController.swift
+++ 100DaysProject1/DetailViewController.swift
@@ -17,7 +17,8 @@ class DetailViewController: UIViewController {
         
         title = selectedImage
         navigationItem.largeTitleDisplayMode = .never
-
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
         if let imageToLoad = selectedImage{
             imageView.image = UIImage(named: imageToLoad)
         }
@@ -29,6 +30,16 @@ class DetailViewController: UIViewController {
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         navigationController?.hidesBarsOnTap = false
+    }
+    @objc func shareTapped(){
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8)else
+        {
+            print("no image found")
+            return
+        }
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
